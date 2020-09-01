98b3027e01059164e996cfc884308fee605c05bf
Carlos Giomar <ardethcgoetia@gmail.com>
2020-09-01 10:36:29 +0100

    Printing possible moves

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -9,14 +9,16 @@ public class King extends ChessPiece{
 	public King(Board board, Color color) {
 		super(board, color);
 	}
-	
+
+	@Override
 	public String toString() {
 		return "K";
 	}
 
 	@Override
 	public boolean[][] possibleMoves() {
-		boolean [][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		return mat;
 	}
+	
 }
